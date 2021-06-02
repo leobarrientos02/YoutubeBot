32d2df797b7441e65ea65f39967c98c2ed0862e8
Your Name <leobarrientos02@gmail.com>
2021-06-02 18:06:33 -0400

    Youtube Bot
    Python code that opens a Youtube Url and searches for a video related to what the user writes in the parameter

---
+++ youtube_bot.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class YoutubeBot:
+    def __init__(self):
+        self.bot = webdriver.Firefox()
+
+    def surf(self):
+        bot = self.bot
+        bot.get('https://www.youtube.com/')
+        time.sleep(3)
+
+    def search(self, video):
+        bot = self.bot
+        bot.get('https://www.youtube.com/results?search_query='+video)
+        time.sleep(2)
+        for i in range(1, 3):
+            bot.execute_script('window.scrollTo(2,document.body.scrollHeight)')
+            time.sleep(2)
+
+
+user = YoutubeBot()
+user.surf()
+video = "UFC Embedded"
+user.search(video)
